9a793194768539faba929bcea53dd3251eaed444
N0bl3 <alexandre.noble.pro@gmail.com>
2016-07-01 04:56:32 +0200

    Provides An image search interface as stated in FCC project.

--- server.js
+++ server.js
@@ -1,84 +1,50 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+const express = require('express');
+const request = require('request');
+const app = express();
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
-
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+const latest = [];
+app.get('/', (req, res) => {
+  res.redirect('/imagesearch/hello?offset=1');
 });
+app.get('/imagesearch/:query', (req, res) => {
+  const offset = Number(req.query.offset) <= 0 ? Number(req.query.offset) : 1;
+  request(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CX}&q=${req.params.query}&safe=high&num=10&searchType=image&start=${offset}`,
+    (error, response) => {
+      if (!error) {
+        const body = JSON.parse(response.body);
+        const items = [];
+
+        latest.unshift({
+          term: req.params.query,
+          when: Date.now(),
+        });
+
+        while (latest.length > 10) {
+          latest.pop();
+        }
+
+
+        body.items.forEach((item) => {
+          items.push({
+            link: item.link,
+            snippet: item.snippet,
+            context: item.image.contextLink,
+            thumbnail: item.image.thumbnailLink,
+          });
+        });
+        res.send(items);
+      }else {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    });
+});
+
+app.get('/latest', (req, res) => {
+  res.send(latest);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server starting on port : ${process.env.PORT || 3000}`);
+});
+
